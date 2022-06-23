6ec894e2961e439e4d3ac83e62ba9929a5d76670
lp-marina-DPS-Technical <105492626+lp-marina-DPS-Technical@users.noreply.github.com>
2022-06-23 15:12:26 +0100

    Update locktimes.js

--- js/locktimes.js
+++ js/locktimes.js
@@ -2,51 +2,51 @@ const d = new Date();
 let month = d.getMonth();
 
 if (month == 1) {
-    document.getElementById('lock').innerHTML += '<object src="../locktimes/1.22.pdf" type="application/pdf" frameborder="0" width="100%" height="100%"></object>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1UHoOJ0M2aFq7F0-T_5XB3r5ZvGLLDXam/preview" frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/1.png" alt="qr1" width="100%" height="100%">';
 }
 if (month == 2) {
-    document.getElementById('lock').innerHTML += '<object data="../locktimes/2.22.pdf" type="application/pdf" frameborder="0" width="100%" height="100%"></object>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/135Lx61s6lOaOerOdZJnNh_J01tqM_s9X/preview" frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/2.png" alt="qr2" width="100%" height="100%">';
 }
 if (month == 3) {
-    document.getElementById('lock').innerHTML += '<object data="../locktimes/3.22.pdf" type="application/pdf" frameborder="0" width="100%" height="100%"></object>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1bXBmWniVo0RAykSXDZcjMeZeOO4unIVB/preview" frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/3.png" alt="qr3" width="100%" height="100%">';
 }
 if (month == 4) {
-    document.getElementById('lock').innerHTML += '<object data="../locktimes/4.22.pdf" type="application/pdf" frameborder="0" width="100%" height="100%"></object>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1MaJAiwVdgY8d-f5IwxDi-Np3mGAO4A9L/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/4.png" alt="qr4" width="100%" height="100%">';
 }
 if (month == 5) {
-    document.getElementById('lock').innerHTML += '<object data="../locktimes/5.22.pdf" type="application/pdf" frameborder="0" width="100%" height="100%"></object>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1WguzXwOs6W5jGaP7NFvfRYXzG6jKxcpv/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/5.png" alt="qr5" width="100%" height="100%">';
 }
 if (month == 6) {
-    document.getElementById('lock').innerHTML += '<object data="../locktimes/6.22.pdf" type="application/pdf" frameborder="0" width="100%" height="100%"></object>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1KPKAshcbWw51wwWVa0zxuacti712ae2s/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/6.png" alt="qr6" width="100%" height="100%">';
 }
 if (month == 7) {
-    document.getElementById('lock').innerHTML += '<object data="../locktimes/7.22.pdf" type="application/pdf" frameborder="0" width="100%" height="100%"></object>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1LMzCnxbgPDi7Xiho1nGGXRM1_rw4d-Pu/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/7.png" alt="qr7" width="100%" height="100%">';
 }
 if (month == 8) {
-    document.getElementById('lock').innerHTML += '<object data="../locktimes/8.22.pdf" type="application/pdf" frameborder="0" width="100%" height="100%"></object>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1as5_D1-Pfay57S6a8Pv2vkWpxKBeKUop/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/8.png" alt="qr8" width="100%" height="100%">';
 }
 if (month == 9) {
-    document.getElementById('lock').innerHTML += '<object data="../locktimes/9.22.pdf" type="application/pdf" frameborder="0" width="100%" height="100%"></object>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1ldoTvoGFVHbEpsvLFm1x_PRkonzsVfpw/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/9.png" alt="qr9" width="100%" height="100%">';
 }
 if (month == 10) {
-    document.getElementById('lock').innerHTML += '<object data="../locktimes/10.22.pdf" type="application/pdf" frameborder="0" width="100%" height="100%"></object>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/10B0A2ARQvvQ2FMjBFSklkId73LAZrBrh/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/10.png" alt="qr10" width="100%" height="100%">';
 }
 if (month == 11) {
-    document.getElementById('lock').innerHTML += '<object data="../locktimes/11.22.pdf" type="application/pdf" frameborder="0" width="100%" height="100%"></object>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1stj8VSoy-pOlqXxchXdw5z21BjKNaiXm/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/11.png" alt="qr11" width="100%" height="100%">';
 }
 if (month == 12) {
-    document.getElementById('lock').innerHTML += '<object data="../locktimes/12.22.pdf" type="application/pdf" frameborder="0" width="100%" height="100%"></object>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1g1DwcFrFQGI6gv9CaiVsVUcPrducIqn_/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/12.png" alt="qr12" width="100%" height="100%">';
 
 }
