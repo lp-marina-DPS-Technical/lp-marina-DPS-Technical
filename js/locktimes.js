dce880e8b19cd26690c6dfb51da6ed70c64bd378
lp-marina-DPS-Technical <105492626+lp-marina-DPS-Technical@users.noreply.github.com>
2023-01-25 10:46:49 +0000

    Update locktimes.js

--- js/locktimes.js
+++ js/locktimes.js
@@ -7,47 +7,47 @@ if (month == 1) {
     document.getElementById('qr').innerHTML += '<img src="../qr/1.png" alt="qr1" width="100%" height="100%">';
 }
 if (month == 2) {
-    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/135Lx61s6lOaOerOdZJnNh_J01tqM_s9X/preview" frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1oNeyEMGlPm3Fy0_-k49yZYkrxMwWydoz/preview" frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/2.png" alt="qr2" width="100%" height="100%">';
 }
 if (month == 3) {
-    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1bXBmWniVo0RAykSXDZcjMeZeOO4unIVB/preview" frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/150SaTGL8jhrSqFwwuhxudRP0hEzdXNQD/preview" frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/3.png" alt="qr3" width="100%" height="100%">';
 }
 if (month == 4) {
-    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1MaJAiwVdgY8d-f5IwxDi-Np3mGAO4A9L/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/19VfjhzsivAQpNXksmALBSO5Pg4M2DIrL/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/4.png" alt="qr4" width="100%" height="100%">';
 }
 if (month == 5) {
-    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1WguzXwOs6W5jGaP7NFvfRYXzG6jKxcpv/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1tOiPm4Axkk15oAzNBlxjc52cHoWirTsz/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/5.png" alt="qr5" width="100%" height="100%">';
 }
 if (month == 6) {
-    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1KPKAshcbWw51wwWVa0zxuacti712ae2s/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1-N2i5ncZbFABQcknMg2XgYFqapxfH_Y5/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/6.png" alt="qr6" width="100%" height="100%">';
 }
 if (month == 7) {
-    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1LMzCnxbgPDi7Xiho1nGGXRM1_rw4d-Pu/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1qPDnTg8OSqvR0CDYe29Cl-3JhxUaEQBx/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/7.png" alt="qr7" width="100%" height="100%">';
 }
 if (month == 8) {
-    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1as5_D1-Pfay57S6a8Pv2vkWpxKBeKUop/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1uhyBWPinjJq_9jpTeMynqoAzIbiefW4H/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/8.png" alt="qr8" width="100%" height="100%">';
 }
 if (month == 9) {
-    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1ldoTvoGFVHbEpsvLFm1x_PRkonzsVfpw/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/17QdTVJnqPva5-JKNrnAaHZtXb9gMTmKl/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/9.png" alt="qr9" width="100%" height="100%">';
 }
 if (month == 10) {
-    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/10B0A2ARQvvQ2FMjBFSklkId73LAZrBrh/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1yWrOhz9o0mqgJSqG0Td9xaCeA86V1Hp5/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/10.png" alt="qr10" width="100%" height="100%">';
 }
 if (month == 11) {
-    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1stj8VSoy-pOlqXxchXdw5z21BjKNaiXm/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1UZ6nK6f4ZojhovxqRR0zw_ZuUcP0C7j5/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/11.png" alt="qr11" width="100%" height="100%">';
 }
 if (month == 12) {
-    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1g1DwcFrFQGI6gv9CaiVsVUcPrducIqn_/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
+    document.getElementById('lock').innerHTML += '<iframe src="https://drive.google.com/file/d/1KIgKaSi1TThpT5qDFwx65sLDxIr9sr8j/preview"  frameborder="0" width="100%" height="100%" allow="autoplay"></iframe>';
     document.getElementById('qr').innerHTML += '<img src="../qr/12.png" alt="qr12" width="100%" height="100%">';
 
 }
